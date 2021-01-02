397a7e26c53e1e535ae6924973a44fcc6a33d6e8
Henry-PC <fanghenry523@gmail.com>
2021-01-01 17:48:50 -0800

    finihsed reverse_number

--- Reverse Integer.js
+++ Reverse Integer.js
@@ -1,3 +1,11 @@
+/**
+ * Given a 32-bit signed integer, reverse digits of an integer.
+ * Note:
+ * Assume we are dealing with an environment that could only store integers 
+ * within the 32-bit signed integer range: [−231,  231 − 1]. 
+ * For the purpose of this problem, assume that your function returns 0 
+ * when the reversed integer overflows.
+ */
 var a = -1463847412;
 
 var reverse = function(x) {
@@ -19,6 +27,7 @@ var reverse = function(x) {
 
     return result;
 };
+
 console.log(reverse(a));
 console.log(Math.pow(2, 31) - 1);
 console.log(-1 * Math.pow(2, 31));
